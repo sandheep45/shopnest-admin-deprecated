74397b649189d451a3ff4ad4c6c2503b5d486726
sandheep45 <budtut143@gmail.com>
2023-05-04 02:13:15 +0530

    add product page almost complete

--- src/pages/add-product.tsx
+++ src/pages/add-product.tsx
@@ -5,13 +5,36 @@ import DropDown from "@src/components/common/DropDown";
 import { useThemeContext } from "@src/context/ThemeContextProvider";
 import { productStatusOptions } from "@src/utils/constants";
 import { CiEdit } from "react-icons/ci";
-import { IoCloudUpload } from "react-icons/io5";
 import Tagify from "@src/components/common/Tagify";
-import Input from "@src/components/common/Input";
-import TextArea from "@src/components/common/TextArea";
+import useTabs, { type ITabComponentProps } from "@src/hooks/useTabs";
+import General from "@src/components/Pages/Product/General";
+import Advanced from "@src/components/Pages/Product/Advanced";
+
+const MAIN_TABS = ["General", "Advanced"];
+
+const MainTabButton: React.FC<ITabComponentProps> = ({
+  isCurrentTab,
+  tab,
+  onClick,
+}) => (
+  <button
+    onClick={onClick}
+    className={`text-lg hover:underline hover:decoration-blue-600 hover:underline-offset-[16px] ${
+      isCurrentTab
+        ? "text-blue-600 underline underline-offset-[16px]"
+        : "text-gray-500"
+    }`}
+  >
+    {tab}
+  </button>
+);
 
 const AddProduct = () => {
   const { isDarkTheme } = useThemeContext();
+  const [Tabs, currentTabIndex] = useTabs({
+    TabComponent: MainTabButton,
+    tabs: MAIN_TABS,
+  });
   const statusOption = useMemo(() => productStatusOptions, []);
 
   return (
@@ -102,86 +125,19 @@ const AddProduct = () => {
       <div className="flex flex-1 flex-col gap-10">
         {/* tabs */}
         <div className="flex gap-5">
-          <span className="text-lg text-blue-600 underline underline-offset-[16px]">
-            General
-          </span>
-          <span className="text-lg text-gray-500">Advanced</span>
+          <Tabs />
         </div>
 
-        <div className="flex w-full flex-col gap-6">
-          {/* general */}
-          <Card className="w-full flex-col gap-8 px-8 pb-12 pt-8">
-            <h3 className="text-xl font-semibold">General</h3>
-
-            <Input
-              descriptionTag="A product name is required and recommended to be unique."
-              label="Product Name"
-              id="product-name"
+        <div className="flex-1">
+          <div className="relative flex-1">
+            <General
+              isCurrentTab={currentTabIndex === 0}
+              statusOption={statusOption}
             />
-
-            <TextArea
-              descriptionTag="Set a description to the product for better visibility."
-              lable="Description"
-              id="description"
-            />
-          </Card>
-
-          {/* media */}
-          <Card className="w-full flex-col gap-8 px-8 pb-12 pt-8">
-            <h3 className="text-xl font-semibold">Media</h3>
-
-            <div className="flex w-full items-center gap-10 rounded-md border border-dashed border-blue-500 bg-blue-500/10 px-10 py-7">
-              <IoCloudUpload className="scale-[2] text-blue-500/60" />
-              <div className="flex flex-col">
-                <h6 className="text-lg font-semibold ">
-                  Drop files here or click to upload.
-                </h6>
-                <span className="text-sm dark:text-gray-500">
-                  Upload up to 10 images. Only *.png, *.jpg and *.jpeg image
-                </span>
-              </div>
-            </div>
-          </Card>
-
-          {/* pricing */}
-          <Card className="w-full flex-col gap-8 px-8 pb-12 pt-8">
-            <h3 className="text-xl font-semibold">Pricing</h3>
-
-            <Input
-              descriptionTag="Base price is the original price of the product."
-              label="Base Price"
-              id="price"
-            />
-
-            <div className="relative flex w-full flex-col gap-2">
-              <span>Discount Price</span>
-              <div className="flex w-full gap-7">
-                <div className="flex-1 rounded-md border border-dashed border-blue-500 bg-blue-500/10 p-5 ">
-                  No Discount
-                </div>
-                <div className="flex-1 rounded-md border border-dashed border-blue-500 bg-blue-500/10 p-5 ">
-                  Percentage %
-                </div>
-                <div className="flex-1 rounded-md border border-dashed border-blue-500 bg-blue-500/10 p-5 ">
-                  Fixed Price
-                </div>
-              </div>
-            </div>
-
-            <div className="flex w-full gap-7">
-              <DropDown
-                descriptionTag="Set the product tax class."
-                className="flex-1"
-                label="Tax Class"
-                list={statusOption}
-              />
-              <Input
-                descriptionTag="Set the product VAT about."
-                className="flex-1"
-                label="VAT Amount (%)"
-              />
-            </div>
-          </Card>
+          </div>
+          <div className="relative flex-1">
+            <Advanced isCurrentTab={currentTabIndex === 1} />
+          </div>
         </div>
       </div>
     </div>
