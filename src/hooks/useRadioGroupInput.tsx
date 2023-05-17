950f7cd16e60e4eca6a32dcb523b9b55c8e3bf22
sudeep sharma <sudip777sharma@gmail.com>
2023-05-17 20:26:16 +0530

    fixed error

--- src/hooks/useRadioGroupInput.tsx
+++ src/hooks/useRadioGroupInput.tsx
@@ -22,36 +22,40 @@ const useRadioGroupInput: UseRadioGroupInput = ({ options, }) => {
             <>
                 <RadioGroup.Root
                     className="flex flex-col gap-2.5"
-                    onValueChange={() => console.log(RadioGroup.Indicator)}
                 >
                     {
                         options.map((item, indx) => (
-                            <>
-                                <div key={`${item.label}${indx}`} className="w-full h-full flex items-center justify-center gap-3">
-                                    <span
-                                        className="w-12 h-12 flex items-center justify-center"
+                            <div
+                                key={`${indx}`}
+                                className="w-full h-full flex items-center justify-center gap-3"
+                            >
+                                <span
+                                    className="w-12 h-12 flex items-center justify-center"
+                                >
+                                    <RadioGroup.Item
+                                        className="bg-[#009ef7] w-[25px] h-[25px] rounded-full outline-none cursor-default"
+                                        value={item.label}
                                     >
-                                        <RadioGroup.Item
-                                            className="bg-[#009ef7] w-[25px] h-[25px] rounded-full outline-none cursor-default"
-                                            value={item.label}
-                                            id={item.label}
-                                        >
-                                            <RadioGroup.Indicator
-                                                className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-white" />
-                                        </RadioGroup.Item>
-                                    </span>
-                                    <span
-                                        className="w-fit h-fit flex flex-col items-start justify-start"
+                                        <RadioGroup.Indicator
+                                            className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-white" />
+                                    </RadioGroup.Item>
+                                </span>
+                                <span
+                                    className="w-fit h-fit flex flex-col items-start justify-start"
+                                >
+                                    <label
+                                        className=""
+                                        htmlFor="r1"
                                     >
-                                        <label className="" htmlFor="r1">
-                                            {item.label}
-                                        </label>
-                                        <p
-                                            className=""
-                                        >{item.descriptionTag}</p>
-                                    </span>
-                                </div>
-                            </>
+                                        {item.label}
+                                    </label>
+                                    <p
+                                        className=""
+                                    >
+                                        {item.descriptionTag}
+                                    </p>
+                                </span>
+                            </div>
                         ))
                     }
                 </RadioGroup.Root>
